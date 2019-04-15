14df10fa1712132a95196d3068c82faef4541b24
ornoyman <or.noyman@citi.com>
2019-04-15 13:05:18 +0300

    enable pkcs#11 backed by SoftHSM2 for CA server

---
+++ network/build-fabric-ca-docker.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e
+
+MY_PATH="`dirname \"$0\"`"              # relative
+MY_PATH="`( cd \"$MY_PATH\" && pwd )`"  # absolutized and normalized
+if [ -z "$MY_PATH" ] ; then
+  # error; for some reason, the path is not accessible
+  # to the script (e.g. permissions re-evaled after suid)
+  exit 1  # fail
+fi
+
+TMP=`mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir'`
+echo "Build tmp directory is $TMP ..."
+
+export GOPATH=$TMP
+
+mkdir -p $GOPATH/src/github.com/hyperledger/
+cd $GOPATH/src/github.com/hyperledger/
+git clone https://gerrit.hyperledger.org/r/fabric-ca
+cd fabric-ca
+git config advice.detachedHead false
+# Fabric-ca release-1.4 (Jan 10, 2019)
+git checkout 27fbd69ab2d0f07212b382eb04aa85c904d2c300
+
+cd $GOPATH/src/github.com/hyperledger/fabric-ca
+
+git config user.name "jenkins"
+git config user.email jenkins@jenkins.com
+
+git am $MY_PATH/add_go_tags.patch
+
+make clean
+FABRIC_CA_DYNAMIC_LINK=true GO_TAGS=pkcs11 make docker
+
+chmod -R +rw $TMP
+
+rm -Rf $TMP
